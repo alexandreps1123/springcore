1a39c88e383aacd61b20e4602caee6b5464414c9
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-02-27 14:06:59 -0300

    properties - create, configure and test

---
+++ src/main/java/com/parkus/properties/CountriesAndLanguages.java
@@ -0,0 +1,25 @@
+package com.parkus.properties;
+
+import java.util.Properties;
+
+public class CountriesAndLanguages {
+    
+    private Properties countryAndLangs;
+
+    public Properties getCountryAndLangs() 
+    {
+        return countryAndLangs;
+    }
+
+    public void setCountryAndLangs(Properties countryAndLangs) 
+    {
+        this.countryAndLangs = countryAndLangs;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "CountriesAndLanguages [countryAndLangs=" + countryAndLangs + "]";
+    }
+
+}
