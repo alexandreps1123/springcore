f8402a7fbde00b6a5c9adc762d521c1e8fcc0d05
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-03-08 15:04:15 -0300

    multiple values

--- src/main/java/com/parkus/propertyplaceholder/MyDAO.java
+++ src/main/java/com/parkus/propertyplaceholder/MyDAO.java
@@ -3,14 +3,18 @@ package com.parkus.propertyplaceholder;
 public class MyDAO {
     
     private String dbServer;
+    private String dbUser;
+    private String dbPass;
 
-    MyDAO(String dbServer) {
+    MyDAO(String dbServer, String dbUser, String dbPass) {
         this.dbServer = dbServer;
+        this.dbUser = dbUser;
+        this.dbPass = dbPass;
     }
 
     @Override
     public String toString() {
-        return "MyDAO [dbServer=" + dbServer + "]";
+        return "MyDAO [dbServer=" + dbServer + ", dbUser=" + dbUser + ", dbPass=" + dbPass + "]";
     }
-    
+
 }
